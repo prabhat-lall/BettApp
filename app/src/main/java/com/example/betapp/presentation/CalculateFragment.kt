fa4d4cb94d4c2e7c8d066f70c6c61704529d68aa
Prabhat Kumar <pkrlal883@gmail.com>
2024-06-23 12:34:18 +0530

    New commit with latest only one commit after changes

---
+++ app/src/main/java/com/example/betapp/presentation/CalculateFragment.kt
@@ -0,0 +1,39 @@
+package com.example.betapp.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.betapp.R
+
+
+
+class CalculateFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calculate, container, false)
+    }
+
+    companion object {
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CalculateFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
