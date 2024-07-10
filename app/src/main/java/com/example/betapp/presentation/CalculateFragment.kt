921f82319836908ad712676fa70b6a9602122edb
Prabhat Kumar <pkrlal883@gmail.com>
2024-07-10 11:45:04 +0530

    necessary commit2

--- app/src/main/java/com/example/betapp/presentation/CalculateFragment.kt
+++ app/src/main/java/com/example/betapp/presentation/CalculateFragment.kt
@@ -30,7 +30,6 @@ class CalculateFragment : Fragment() {
     }
 
     companion object {
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             CalculateFragment().apply {
