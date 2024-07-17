ad87b8871923c7e7d8adcb3782e3b47467ddc8ea
Prabhat Kumar <pkrlal883@gmail.com>
2024-07-17 22:27:35 +0530

    new commit 1

--- app/src/main/java/com/example/betapp/presentation/CalculateFragment.kt
+++ app/src/main/java/com/example/betapp/presentation/CalculateFragment.kt
@@ -35,6 +35,7 @@ class CalculateFragment : Fragment() {
             CalculateFragment().apply {
                 arguments = Bundle().apply {
                     var a = "Unnacessary commit"
+                    //comment 1
                 }
             }
     }
